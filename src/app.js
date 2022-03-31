2da454d6a2ca468eba7f6665c22c1f1eddd4a643
Boris Belmar <borisbelmarm@gmail.com>
2022-03-30 22:41:57 -0300

    Add partials support and dotenv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const hbs = require('hbs')
 
 const router = require('./router')
 
 const app = express()
 
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
